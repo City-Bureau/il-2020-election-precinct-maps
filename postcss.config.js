867fcdee0ec4ef52056a19b96089b688f615e88c
pjsier <pjsier@gmail.com>
2020-11-30 11:29:56 -0600

    refactor: cleanup styles

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   plugins: [
-    require("postcss-import"),
     require("autoprefixer"),
     // TODO: For some reason build not working with cssnano
     // ...(process.env.NODE_ENV === "production" ? [require("cssnano")] : []),
--- src/js/entry.js
+++ src/js/entry.js
@@ -1,9 +1,3 @@
 import "formdata-polyfill"
 import "classlist-polyfill"
-import cssVars from "css-vars-ponyfill"
 import "./index.js"
-
-cssVars({
-  preserveVars: true,
-  watch: true,
-})
